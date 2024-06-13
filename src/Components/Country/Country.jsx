a7ceae50caaaf870ce3afb360447b0f0d0d54c0e
Sohel Rana <sohelrana130523@gmail.com>
2024-06-13 01:12:54 +0100

    Buton added to every components

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
   console.log(country);
   const {name, flags} = country;
+  const [visit, setVisit] = useState(true);
+  function countryVisited(){
+    setVisit(!visit);
+  }
   return (
     <div className='country'>
       <p>Name: {name?.common}</p>
-      <img src={flags.png} alt="" />
+      <img src={flags.png} alt="" /><br />
+      <button onClick={countryVisited}>{visit ? 'Visited': 'Need to go'}</button>
+      <p>{visit&&'I have visted.'}</p>
     </div>
   );
 };
